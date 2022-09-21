c57037164d543a4a5acd0266efe682936a9f3292
Pedro Lucas <pedro.lmarinho2002@gmail.com>
2022-09-21 17:12:57 -0300

    add: função de ler regras de negocio

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import re
 
 def leArquivo(arquiv):
 	arquivo = open(arquiv, "r")
@@ -9,9 +10,43 @@ def leArquivo(arquiv):
 	arquivo.close()
 	return automato
 
+def leRegrasDeTransicao(automato):
+	regras = []
+	for i in range(1, len(automato)):
+		automato[i] = automato[i].strip("\n").replace(" ", "").split(",")
+		regras.append(automato[i])
+	return regras
 
 
 
-automato = leArquivo("afd.txt")
 
-print(automato)
+
+
+
+
+
+automato = leArquivo("afd.txt") 
+
+regras = leRegrasDeTransicao(automato)
+
+
+for i in len(regras):
+	print(regras[i])
+
+
+palavra = input("Digite uma palavra: ")
+
+
+
+
+""" print(automato[1])
+
+for c in automato[0]:
+	if c == "D":
+		print("Achei") """
+
+
+
+
+
+
